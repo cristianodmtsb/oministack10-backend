f34b05ae40cb047327362a1c11f1385c3e2cbbc9
Cristiano Castro <cristianodmtsb@gmail.com>
2020-01-17 10:25:29 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const http = require("http");
+const routes = require("./routes");
+const { setupWebsocket } = require("./websocket");
+require("dotenv").config();
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect(process.env.BASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
